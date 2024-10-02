47374f51c05fb270feb260629589ae3577ad7da8
Un1q32 <joey.t.reinhart@gmail.com>
2024-10-02 10:36:18 -0400

    more error checks

--- src/stdio/puts.c
+++ src/stdio/puts.c
@@ -2,5 +2,8 @@
 #include <string.h>
 
 int puts(const char *str) {
-  return fwrite(str, 1, strlen(str), stdout) + fwrite("\n", 1, 1, stdout);
+  if (stdout)
+    return fwrite(str, 1, strlen(str), stdout) + fwrite("\n", 1, 1, stdout);
+  else
+    return EOF;
 }

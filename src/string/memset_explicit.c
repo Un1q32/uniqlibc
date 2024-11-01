202130538651ea1a136bcc93545243a5c341767f
Un1q32 <joey.t.reinhart@gmail.com>
2024-11-01 19:45:07 -0400

    fix memset return

--- src/string/memset_explicit.c
+++ src/string/memset_explicit.c
@@ -1,6 +1,7 @@
 #include <string.h>
 
 void *memset_explicit(void *buf, int c, size_t n) {
-  memset(buf, c, n);
+  void *ret = memset(buf, c, n);
   __asm__ volatile("" : : "r"(buf) : "memory");
+  return ret;
 }

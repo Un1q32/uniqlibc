f5d82cf7d0c78cfe3319a78d4d5c2e03549e5bdf
Un1q32 <joey.t.reinhart@gmail.com>
2024-11-01 16:25:40 -0400

    rewrite strtoimax

--- src/inttypes/strtoimax.c
+++ src/inttypes/strtoimax.c
@@ -1,9 +1,62 @@
+#include <ctype.h>
 #include <inttypes.h>
+#include <stdbool.h>
 
 intmax_t strtoimax(const char *nptr, char **endptr, int base) {
+  while (isspace(*nptr))
+    ++nptr;
+
+  bool negative = false;
   if (nptr[0] == '-') {
-    uintmax_t ret = strtoumax(nptr + 1, endptr, base);
-    return -ret;
-  } else
-    return strtoumax(nptr, endptr, base);
+    negative = true;
+    ++nptr;
+  } else if (nptr[0] == '+')
+    ++nptr;
+
+  if (base == 0) {
+    if (nptr[0] == '0') {
+      if (nptr[1] == 'x' || nptr[1] == 'X') {
+        base = 16;
+        nptr += 2;
+      } else if (nptr[1] == 'b' || nptr[1] == 'B') {
+        base = 2;
+        nptr += 2;
+      } else {
+        base = 8;
+        nptr += 1;
+      }
+    } else
+      base = 10;
+  } else if (base == 16) {
+    if (nptr[0] == '0' && (nptr[1] == 'x' || nptr[1] == 'X'))
+      nptr += 2;
+  } else if (base == 2) {
+    if (nptr[0] == '0' && (nptr[1] == 'b' || nptr[1] == 'B'))
+      nptr += 2;
+  }
+
+  intmax_t num = 0;
+
+  while (1) {
+    char digit;
+    if (*nptr >= '0' && *nptr <= '9')
+      digit = *nptr - '0';
+    else if (*nptr >= 'a' && *nptr <= 'z')
+      digit = *nptr - 'a' + 10;
+    else if (*nptr >= 'A' && *nptr <= 'Z')
+      digit = *nptr - 'A' + 10;
+    else
+      break;
+    if (digit >= base)
+      break;
+
+    num = num * base + digit;
+    ++nptr;
+  }
+
+  if (negative)
+    num = -num;
+  if (endptr)
+    *endptr = (char *)nptr;
+  return num;
 }
--- src/inttypes/strtoumax.c
+++ src/inttypes/strtoumax.c
@@ -1,38 +1,5 @@
-#include <stddef.h>
-#include <stdint.h>
+#include <inttypes.h>
 
 uintmax_t strtoumax(const char *nptr, char **endptr, int base) {
-  uintmax_t ret = 0;
-  size_t i = 0;
-  while (nptr[i] == ' ')
-    i++;
-  if (nptr[i] == '+')
-    i++;
-  if (base == 0) {
-    if (nptr[i] == '0') {
-      if (nptr[i + 1] == 'x' || nptr[i + 1] == 'X') {
-        base = 16;
-        i += 2;
-      } else
-        base = 8;
-    } else
-      base = 10;
-  }
-  if (base == 16 && nptr[i] == '0' &&
-      (nptr[i + 1] == 'x' || nptr[i + 1] == 'X'))
-    i += 2;
-  while (1) {
-    if (nptr[i] >= '0' && nptr[i] <= '9') {
-      ret = ret * base + nptr[i] - '0';
-    } else if (nptr[i] >= 'a' && nptr[i] <= 'z') {
-      ret = ret * base + nptr[i] - 'a' + 10;
-    } else if (nptr[i] >= 'A' && nptr[i] <= 'Z') {
-      ret = ret * base + nptr[i] - 'A' + 10;
-    } else
-      break;
-    i++;
-  }
-  if (endptr)
-    *endptr = (char *)&nptr[i];
-  return ret;
+  return strtoimax(nptr, endptr, base);
 }

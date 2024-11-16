e02962a587f56ba425286897800c21ef66fa5d6f
Un1q32 <joey.t.reinhart@gmail.com>
2024-11-15 23:01:36 -0500

    unfinished riscv freebsd support

--- include/machine/limits.h
+++ include/machine/limits.h
@@ -5,6 +5,8 @@
 #include <machine/arm/limits.h>
 #elif defined(__i386__) || defined(__x86_64__)
 #include <machine/x86/limits.h>
+#elif defined(__riscv)
+#include <machine/riscv/limits.h>
 #elif defined(__powerpc__) || defined(__POWERPC__)
 #include <machine/powerpc/limits.h>
 #else
--- include/machine/param.h
+++ include/machine/param.h
@@ -5,6 +5,8 @@
 #include <machine/arm/param.h>
 #elif defined(__i386__) || defined(__x86_64__)
 #include <machine/x86/param.h>
+#elif defined(__riscv)
+#include <machine/riscv/param.h>
 #elif defined(__powerpc__) || defined(__POWERPC__)
 #include <machine/powerpc/param.h>
 #else

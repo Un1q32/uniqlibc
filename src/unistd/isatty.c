438f2b69739db1222dc22d2262a69c0a7876e1cc
Un1q32 <joey.t.reinhart@gmail.com>
2024-11-18 12:51:47 -0500

    simplify isatty

--- src/unistd/isatty.c
+++ src/unistd/isatty.c
@@ -1,12 +1,6 @@
-#include <errno.h>
 #include <sys/ioctl.h>
 
 int isatty(int fd) {
   struct winsize wsz;
-  unsigned long r = ioctl(fd, TIOCGWINSZ, &wsz);
-  if (r == 0)
-    return 1;
-  if (errno != EBADF)
-    errno = ENOTTY;
-  return 0;
+  return ioctl(fd, TIOCGWINSZ, &wsz);
 }

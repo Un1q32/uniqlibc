ea353dd35e8590e51b880a14a968d93a3231d5bc
Un1q32 <joey.t.reinhart@gmail.com>
2024-12-06 14:38:16 -0500

    add comments to cstart.c

--- crt/cstart.c
+++ crt/cstart.c
@@ -13,6 +13,13 @@ const char **NXArgv = NULL;
 const char **environ = NULL;
 const char *__progname = NULL;
 
+/*
+ * This function is the first real code run after the program starts, called by
+ * the entry function It sets up argc, argv, and envp, a couple other global
+ * variables, sets the correct buffer mode for stdout, initializes the stack
+ * protector guard, and calls main()
+ */
+
 void __cstart(const char *sp) {
   NXArgc = *(int *)sp;
   sp += sizeof(char *);
@@ -20,6 +27,7 @@ void __cstart(const char *sp) {
   sp += sizeof(char *) * (NXArgc + 1);
   environ = (const char **)sp;
 
+  /* equivalent to basename(argv[0]) */
   if (NXArgv[0] != NULL) {
     __progname = NXArgv[0];
     const char *p;
@@ -29,15 +37,24 @@ void __cstart(const char *sp) {
   } else
     __progname = "";
 
+  /* if stdout is a terminal device it should be line buffered */
   if (isatty(stdout->fd))
     stdout->flags |= __SLBF;
 
 #ifdef __MACH__
+  /*
+   * Darwin has an extra string array stored
+   * after envp that gets passed to main
+   */
   const char **apple = environ;
   while (*apple != NULL)
     apple++;
   apple++;
 
+  /*
+   * One of the strings in the Apple array
+   * is used to initialize the stack guard
+   */
   __stack_protect_init(apple);
 
   exit(main(NXArgc, NXArgv, environ, apple));

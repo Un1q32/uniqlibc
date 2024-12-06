b0d8392e32e03e13af4cac97023a4c7ff4888193
Un1q32 <joey.t.reinhart@gmail.com>
2024-12-06 11:12:05 -0500

    ungetc test error printing

--- tests/ungetc.c
+++ tests/ungetc.c
@@ -7,11 +7,15 @@ int main(void) {
   ungetc('a', stdin);
   fputs("input: ", stdout);
   fflush(stdout);
-  char c[4];
+  char c[5];
   fread(c, 1, 4, stdin);
+  c[4] = '\0';
   if (memcmp(c, "abc", 3) == 0)
     printf("output: %c\n", c[3]);
-  else
+  else {
     puts("ungetc failed");
+    printf("expected: abc%c\n", c[3]);
+    printf("got: %s\n", c);
+  }
   return 0;
 }

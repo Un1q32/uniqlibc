be603e0b00f331cd9fcb3e4155e361aaf0159e10
Un1q32 <joey.t.reinhart@gmail.com>
2024-12-10 16:22:02 +0000

    fix

--- platform/darwin/src/fstat.c
+++ platform/darwin/src/fstat.c
@@ -1,6 +1,5 @@
+#include <sys/stat.h>
 #include <sys/syscall.h>
 #include <unistd.h>
 
-int fstat(int fd, struct stat *st) {
-  return syscall(SYS_fstat, fd, st);
-}
+int fstat(int fd, struct stat *st) { return syscall(SYS_fstat, fd, st); }

dc17291613ec8da1e7322b00f3d0a14b1bcb7ebc
Un1q32 <joey.t.reinhart@gmail.com>
2024-12-13 16:19:36 -0500

    nit

--- src/malloc/calloc.c
+++ src/malloc/calloc.c
@@ -4,7 +4,7 @@
 void *calloc(size_t count, size_t size) {
   size_t total_size = count * size;
   void *ptr = malloc(total_size);
-  if (ptr == NULL)
+  if (!ptr)
     return NULL;
 
   bzero(ptr, total_size);

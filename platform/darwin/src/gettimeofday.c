612354a0c29aed460aee44b1065b7c5ab3a1ebaf
Un1q32 <joey.t.reinhart@gmail.com>
2024-12-13 21:41:29 -0500

    nits

--- platform/darwin/src/gettimeofday.c
+++ platform/darwin/src/gettimeofday.c
@@ -4,8 +4,8 @@
 #include <unistd.h>
 
 int gettimeofday(struct timeval *tv, void *tz) {
-  if (tv != NULL) {
-    long ret = syscall(SYS_gettimeofday, tv, 0);
+  if (tv) {
+    long ret = syscall(SYS_gettimeofday, tv, NULL);
     if (ret == -1)
       return -1;
     else if (ret != 0) {
@@ -13,7 +13,7 @@ int gettimeofday(struct timeval *tv, void *tz) {
       tv->tv_usec = __syscallret2;
     }
   }
-  if (tz != NULL) {
+  if (tz) {
     struct timezone *tmp_tz = (struct timezone *)tz;
     tmp_tz->tz_minuteswest = 0;
     tmp_tz->tz_dsttime = 0;

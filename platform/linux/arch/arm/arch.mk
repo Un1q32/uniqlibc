c4af12d2ca1584eb66dae86c295bb55e28e59cfe
Un1q32 <joey.t.reinhart@gmail.com>
2024-12-17 14:49:22 -0500

    linux arm thing update

--- platform/linux/arch/arm/arch.mk
+++ platform/linux/arch/arm/arch.mk
@@ -1,4 +1,4 @@
-BUILTINS := fixunsdfdi floatundidf udivdi3 umoddi3 umodsi3 modsi3 divsi3 udivsi3 aeabi_div0 aeabi_uldivmod udivmoddi4 aeabi_uidivmod udivmodsi4 floatsidf fixdfsi comparedf2 comparesf2 adddf3 subdf3 muldf3 addsf3 subsf3 mulsf3 divsf3 fp_mode fixsfsi extendsfdf2 truncdfsf2 clzsi2 aeabi_memset
+BUILTINS := fixunsdfdi floatundidf udivdi3 umoddi3 umodsi3 modsi3 divsi3 udivsi3 aeabi_div0 aeabi_uldivmod udivmoddi4 aeabi_uidivmod udivmodsi4 floatsidf fixdfsi comparedf2 comparesf2 adddf3 subdf3 muldf3 divdf3 addsf3 subsf3 mulsf3 divsf3 fp_mode fixsfsi extendsfdf2 truncdfsf2 clzsi2 aeabi_memset
 _BUILTINS := $(addprefix src/builtins/,$(addsuffix .o,$(BUILTINS)))
 BCC = $(V)src=$@; src=$${src\#\#*/}; printf " \033[1;32mCC\033[0m %s\n" "$$src"; $(CC) --sysroot sdk -Iinclude -std=c99 $(CFLAGS) $(OPTFLAGS) -c -o $@ $<
 
@@ -51,6 +51,9 @@ src/builtins/subdf3.o: compiler-rt/lib/builtins/subdf3.c
 src/builtins/muldf3.o: compiler-rt/lib/builtins/muldf3.c
 	$(BCC)
 
+src/builtins/divdf3.o: compiler-rt/lib/builtins/divdf3.c
+	$(BCC)
+
 src/builtins/addsf3.o: compiler-rt/lib/builtins/addsf3.c
 	$(BCC)
 
@@ -78,28 +81,6 @@ src/builtins/fp_mode.o: compiler-rt/lib/builtins/arm/fp_mode.c
 src/builtins/aeabi_memset.o: compiler-rt/lib/builtins/arm/aeabi_memset.S
 	$(BCC)
 
-ifdef NOASM
-
-src/builtins/umodsi3.o: compiler-rt/lib/builtins/umodsi3.c
-	$(BCC)
-
-src/builtins/modsi3.o: compiler-rt/lib/builtins/modsi3.c
-	$(BCC)
-
-src/builtins/divsi3.o: compiler-rt/lib/builtins/divsi3.c
-	$(BCC)
-
-src/builtins/udivsi3.o: compiler-rt/lib/builtins/udivsi3.c
-	$(BCC)
-
-src/builtins/udivmodsi4.o: compiler-rt/lib/builtins/udivmodsi4.c
-	$(BCC)
-
-src/builtins/clzsi2.o: compiler-rt/lib/builtins/clzsi2.c
-	$(BCC)
-
-else
-
 src/builtins/umodsi3.o: compiler-rt/lib/builtins/arm/umodsi3.S
 	$(BCC)
 
@@ -117,5 +98,3 @@ src/builtins/udivmodsi4.o: compiler-rt/lib/builtins/arm/udivmodsi4.S
 
 src/builtins/clzsi2.o: compiler-rt/lib/builtins/arm/clzsi2.S
 	$(BCC)
-
-endif

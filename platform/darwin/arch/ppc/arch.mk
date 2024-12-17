71d19990447b252ebf793284c0d835ed5ea57cb4
Un1q32 <joey.t.reinhart@gmail.com>
2024-12-17 14:24:29 -0500

    fix ppc

--- platform/darwin/arch/ppc/arch.mk
+++ platform/darwin/arch/ppc/arch.mk
@@ -1,4 +1,4 @@
-BUILTINS := udivdi3 umoddi3 fixunstfdi floatditf gcc_qadd gcc_qsub gcc_qmul
+BUILTINS := udivdi3 umoddi3 fixunstfdi floatditf gcc_qadd gcc_qsub gcc_qmul gcc_qdiv
 _BUILTINS := $(addprefix src/builtins/,$(addsuffix .o,$(BUILTINS)))
 BCC = $(V)src=$@; src=$${src\#\#*/}; printf " \033[1;32mCC\033[0m %s\n" "$$src"; $(CC) --sysroot sdk -Iinclude -std=c99 $(CFLAGS) $(OPTFLAGS) -c -o $@ $<
 
@@ -25,4 +25,7 @@ src/builtins/gcc_qsub.o: compiler-rt/lib/builtins/ppc/gcc_qsub.c
 	$(BCC)
 
 src/builtins/gcc_qmul.o: compiler-rt/lib/builtins/ppc/gcc_qmul.c
+	$(BCC)
+
+src/builtins/gcc_qdiv.o: compiler-rt/lib/builtins/ppc/gcc_qdiv.c
 	$(BCC)

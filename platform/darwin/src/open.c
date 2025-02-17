ca13f25af56d5ebc26762f5e392b54478eda9e7c
Un1q32 <joey.t.reinhart@gmail.com>
2025-02-17 10:28:41 -0500

    according to a man page, it's impossible for fcntl to fail here

--- platform/darwin/src/open.c
+++ platform/darwin/src/open.c
@@ -18,11 +18,7 @@ int open(const char *path, int flags, ...) {
     cloexec = true;
   }
   int fd = syscall(SYS_open, path, flags, mode);
-  if (cloexec && fd != -1) {
-    if (fcntl(fd, F_SETFD, FD_CLOEXEC) == -1) {
-      close(fd);
-      return -1;
-    }
-  }
+  if (cloexec && fd != -1)
+    fcntl(fd, F_SETFD, FD_CLOEXEC);
   return fd;
 }

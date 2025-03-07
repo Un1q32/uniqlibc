f574eae7285eb49e1c94bc9393617b9141a993f3
Un1q32 <joey.t.reinhart@gmail.com>
2025-03-06 22:37:45 -0500

    remove comment

--- src/stdlib/arc4random_buf.c
+++ src/stdlib/arc4random_buf.c
@@ -3,9 +3,6 @@
 #include <unistd.h>
 
 void arc4random_buf(void *buf, size_t n) {
-  /* TODO: Apple's man page says their implementation works in chroot */
-  /* ours should too */
-
   int fd = open("/dev/urandom", O_RDONLY);
   if (fd == -1)
     abort();

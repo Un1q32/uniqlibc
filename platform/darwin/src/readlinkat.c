d3c279156da527fe6d7da768ee7c9508e2d0aa91
Un1q32 <joey.t.reinhart@gmail.com>
2025-03-21 17:17:11 -0400

    optimize readlinkat on darwin and bsd

--- platform/darwin/src/readlinkat.c
+++ platform/darwin/src/readlinkat.c
@@ -8,13 +8,13 @@ ssize_t readlinkat(int fd, const char *path, char *buf, size_t bufsize) {
   if (fd == AT_FDCWD || path[0] == '/')
     return readlink(path, buf, bufsize);
 
-  char fdpath[PATH_MAX];
+  char fdpath[PATH_MAX + strlen(path) + 2];
   if (fcntl(fd, F_GETPATH, fdpath) == -1)
     return -1;
 
-  char new_path[strlen(fdpath) + strlen(path) + 2];
-  strcpy(new_path, fdpath);
-  strcat(new_path, "/");
-  strcat(new_path, path);
-  return readlink(new_path, buf, bufsize);
+  if (path[0] != '\0') {
+    strcat(fdpath, "/");
+    strcat(fdpath, path);
+  }
+  return readlink(fdpath, buf, bufsize);
 }

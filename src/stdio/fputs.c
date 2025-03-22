b1e59297f4c2dc71a68ee9c53918a36139429b47
Un1q32 <joey.t.reinhart@gmail.com>
2025-03-22 19:56:24 -0400

    fix

--- src/stdio/fputs.c
+++ src/stdio/fputs.c
@@ -3,6 +3,6 @@
 
 int fputs(const char *restrict str, FILE *restrict stream) {
   if (!str)
-    fputs("(null)", stream);
+    return fputs("(null)", stream);
   return fwrite(str, 1, strlen(str), stream);
 }

330d73be315e35f17613f0f9cb016db79cfa8d7f
Un1q32 <joey.t.reinhart@gmail.com>
2025-05-20 18:55:44 -0400

    correctly return EOF when fflush(NULL) fails

--- src/stdio/fflush.c
+++ src/stdio/fflush.c
@@ -1,24 +1,33 @@
+#include <stdbool.h>
 #include <stdio.h>
 
 int fflush(FILE *stream) {
   if (!stream) {
+    bool error = false;
     if (__open_stream_list) {
       size_t i = *((size_t *)__open_stream_list);
       while (i) {
         if (__open_stream_list[i])
-          fflush(__open_stream_list[i]);
+          if (fflush(__open_stream_list[i]) != 0)
+            error = true;
         i--;
       }
     }
-    fflush(stdin);
-    fflush(stdout);
-    fflush(stderr);
+    if (fflush(stdin) != 0)
+      error = true;
+    if (fflush(stdout) != 0)
+      error = true;
+    if (fflush(stderr) != 0)
+      error = true;
+    if (error)
+      return EOF;
     return 0;
   }
   if (stream->write) {
     if (!stream->writebufcount)
       return 0;
-    ssize_t writeret = stream->write(stream->fd, stream->writebuf, stream->writebufcount);
+    ssize_t writeret =
+        stream->write(stream->fd, stream->writebuf, stream->writebufcount);
     if (writeret == -1 || (size_t)writeret != stream->writebufcount) {
       stream->flags |= __STDIO_ERROR;
       return EOF;

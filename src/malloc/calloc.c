1a13ab2c26e908feeb8ff975ddfe8c3f9cd74f21
Un1q32 <joey.t.reinhart@gmail.com>
2025-07-22 04:30:59 -0400

    fix calloc

--- src/malloc/calloc.c
+++ src/malloc/calloc.c
@@ -1,8 +1,13 @@
+#include <errno.h>
 #include <malloc.h>
 #include <strings.h>
 
 void *calloc(size_t count, size_t size) {
   size_t total_size = count * size;
+  if (total_size / size != count) {
+    errno = ENOMEM;
+    return NULL;
+  }
   void *ptr = malloc(total_size);
   if (!ptr)
     return NULL;

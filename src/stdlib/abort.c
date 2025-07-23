9347ccfd4d83e6ca510a7baf4e26ba6d7c6c858a
Un1q32 <joey.t.reinhart@gmail.com>
2025-07-23 02:31:03 -0400

    improve abort

--- src/stdlib/abort.c
+++ src/stdlib/abort.c
@@ -1,7 +1,15 @@
+#include <signal.h>
 #include <unistd.h>
+
+#if defined(__linux__) || defined(__APPLE__)
+#define SIGABRT 6
+#else
+#error platform not supported
+#endif
 
 void abort(void) {
   /* try to crash */
+  raise(SIGABRT);
   *(volatile char *)0 = 0;
   _exit(127);
 }

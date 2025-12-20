76e3f5b4095aa35242ebaf2fc8fc68bf300cfc98
Un1q32 <joey.t.reinhart@gmail.com>
2025-12-20 02:38:42 -0500

    float16

--- platform/linux/arch/x86_64/arch.mk
+++ platform/linux/arch/x86_64/arch.mk
@@ -1,1 +1,10 @@
-all:
+BUILTINS := extendhfxf2
+_BUILTINS := $(addprefix src/builtins/,$(addsuffix .o,$(BUILTINS)))
+BCC = $(V)src=$@; src=$${src\#\#*/}; printf " \033[1;32mCC\033[0m %s\n" "$$src"; $(CC) --sysroot sdk -Iinclude -std=c99 -fno-stack-protector $(CFLAGS) $(OPTFLAGS) -c -o $@ $<
+
+.PHONY: all
+
+all: $(_BUILTINS)
+
+src/builtins/extendhfxf2.o: compiler-rt/lib/builtins/extendhfxf2.c
+	$(BCC)

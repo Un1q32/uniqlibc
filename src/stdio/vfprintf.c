c862ac4d362332db6a74066837bdf8d18ac94eff
Un1q32 <joey.t.reinhart@gmail.com>
2026-01-06 15:18:48 -0500

    use malloc instead of a vla

--- src/stdio/vfprintf.c
+++ src/stdio/vfprintf.c
@@ -1,12 +1,16 @@
 #include <stdio.h>
+#include <stdlib.h>
 
 int vfprintf(FILE *restrict stream, const char *restrict format, va_list ap) {
   va_list ap2;
   va_copy(ap2, ap);
-  char buf[vsnprintf(NULL, 0, format, ap2) + 1];
+  char *buf = malloc(vsnprintf(NULL, 0, format, ap2) + 1);
   va_end(ap2);
+  if (!buf)
+    return -1;
   int ret = vsnprintf(buf, sizeof(buf), format, ap);
   if (fwrite(buf, 1, ret, stream) != (size_t)ret)
     return -1;
+  free(buf);
   return ret;
 }

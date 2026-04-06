d9f0781fe7635dd5e0b994f003a98c27aa1b0a29
Un1q32 <joey.t.reinhart@gmail.com>
2026-04-06 19:44:18 -0400

    better vsnprintf_chk

--- src/stdio/__vsnprintf_chk.c
+++ src/stdio/__vsnprintf_chk.c
@@ -1,8 +1,14 @@
 #include <stdio.h>
+#include <stdlib.h>
 
 int __vsnprintf_chk(char *str, size_t maxlen, int flag, size_t slen,
                     const char *format, va_list ap) {
   (void)flag;
-  (void)slen;
+
+  if (maxlen > slen) {
+    fputs("Buffer overflow detected!\n", stderr);
+    abort();
+  }
+
   return vsnprintf(str, maxlen, format, ap);
 }

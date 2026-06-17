f82e7bd8fdff62c9073b5db10173118d38fed9b5
Un1q32 <joey.t.reinhart@gmail.com>
2026-06-17 11:22:18 -0400

    fix

--- src/malloc/aligned_alloc.c
+++ src/malloc/aligned_alloc.c
@@ -22,7 +22,7 @@ static bool realloc_heap_list(size_t size, size_t old_size) {
 
 void *aligned_alloc(size_t alignment, size_t size) {
   /* ensure alignment is a power of 2 */
-  if (alignment & (alignment - 1) != 0) {
+  if ((alignment & (alignment - 1)) != 0) {
     errno = EINVAL;
     return NULL;
   }

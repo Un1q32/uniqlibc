b428ade8b98ba6713f9f898dbd3f1a336c94cb23
Un1q32 <joey.t.reinhart@gmail.com>
2026-06-18 14:16:50 -0400

    squeze more memory

--- src/malloc/internal_malloc.c
+++ src/malloc/internal_malloc.c
@@ -2,6 +2,14 @@
 
 void *__internal_malloc(size_t size) {
   size += sizeof(size_t);
+  /* overflow check */
+  if (size < sizeof(size_t) {
+    errno = ENOMEM;
+    return NULL;
+  }
+  /* round up to multiple of page size */
+  if ((size & PAGE_MASK) != 0)
+    size = (size | PAGE_MASK) + 1;
   size_t *ptr =
       mmap(NULL, size, PROT_READ | PROT_WRITE, MAP_PRIVATE | MAP_ANON, -1, 0);
   if (ptr == MAP_FAILED)
